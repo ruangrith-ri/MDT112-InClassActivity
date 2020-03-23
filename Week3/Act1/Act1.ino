f6cd11be2ee779b8c31f0d8415eb796b67f9aa8d
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-03-23 13:57:02 +0700

    blink example

--- Week3/Act1/Act1.ino
+++ Week3/Act1/Act1.ino
@@ -1,3 +1,14 @@
-void setup() {}
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+}
 
-void loop() {}
+void loop() {
+  digitalWrite(2, 1);
+  digitalWrite(3, HIGH);
+  delay(500);
+
+  digitalWrite(2, 0);
+  digitalWrite(3, LOW);
+  delay(500);
+}
