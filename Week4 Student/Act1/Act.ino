108b4e425e6c9fb81578e779bbc7d5f4619c04ee
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-03-27 16:49:05 +0700

    commit act2

---
+++ Week4 Student/Act1/Act.ino
@@ -0,0 +1,11 @@
+void setup(){
+    Serial.begin(9600);
+    Serial.println(sizeof(int));
+    Serial.println(sizeof(5));
+
+    int b = 5;
+    Serial.println(sizeof(b));
+}
+void loop(){
+
+}
---
+++ Week4 Student/Act2/Act2.ino
@@ -0,0 +1,19 @@
+void setup(){
+    
+    int sampleArray[] = {10,20,30,40,50,60,70,80,90,100};
+
+    String stringShowArray;
+
+    for (int i = 0 ; i <= 9 ; i++ ){
+       stringShowArray = stringShowArray + sampleArray[i] + '\t';
+
+    }
+    Serial.begin(9600);
+    Serial.println(stringShowArray);
+
+
+}
+
+void loop(){
+
+}
