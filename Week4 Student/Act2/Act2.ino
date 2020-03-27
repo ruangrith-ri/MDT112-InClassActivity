3fddc8407d0c3ea17757c2cdeb6708201ab095ea
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-03-27 16:57:50 +0700

    InClass2Fin

--- Week4 Student/Act2/Act2.ino
+++ Week4 Student/Act2/Act2.ino
@@ -1,13 +1,15 @@
-void srtup(){  
+void setup(){  
     int ar[] = {10,20,30,40,50,60,70,80,90,100};
 
     String Show ;
 
-    for (int i = 0 ; i <=9 ; i++){
-        Show = Show + ar[i] + '\t';
+    for (int i = 0; i <= 9 ; i++) {
+        Show  = Show  + ar[i] + '\t';
     }
     Serial.begin(9600);
     Serial.println(Show);
+    
+
 }
 void loop(){
 
