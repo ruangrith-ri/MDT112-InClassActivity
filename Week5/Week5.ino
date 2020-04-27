a842fa60e25a0f7f387750e76330c79e4511ce2a
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-04-27 17:00:24 +0700

    Act1

---
+++ Week5/Week5.ino
@@ -0,0 +1,37 @@
+bool laststate = true;
+int x = 0;
+void setup()
+{
+    pinMode(2,INPUT_PULLUP);
+
+    Serial.begin(9600);
+}
+void loop()
+{
+    bool currentSteate = digitalRead(2);
+
+    /* Serial.println("currentSteate : "+String(currentSteate) + "lastState : " + String(laststate));
+
+    laststate = currentSteate; */
+
+    if(laststate > currentSteate)
+    {
+        Serial.println("Falling");
+        tone(8,400,100);
+        delay(100);
+        tone(8,400,100);
+        x++;
+        Serial.println(x);
+    }
+
+    else if (currentSteate != laststate) 
+    {
+        Serial.println("Rising");
+        tone(8,200,1000);
+        
+    }
+    laststate = currentSteate;
+
+    delay(50);
+
+}
