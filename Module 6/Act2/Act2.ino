31eccafb3820d87593f77cec7b42b7a5bdc20a6e
Windwill <jojoeza10@gmail.com>
2020-05-11 14:58:49 +0700

    Act2 update

--- Module 6/Act2/Act2.ino
+++ Module 6/Act2/Act2.ino
@@ -2,7 +2,7 @@ long lasttimeStageChange = 0;
 String state = "ON";
 
 void setup(){
-    
+    Serial.begin(9600);
     pinMode(5,OUTPUT);
 
 }
@@ -10,20 +10,19 @@ void setup(){
 void loop(){
     long currentTime = millis();
 
-    if(state == "ON" && currentTime - lasttimeStageChange >= 500){
-        state = "OFF";
-        lasttimeStageChange = currentTime;
-    }
-    else if(state == "OFF" && currentTime - lasttimeStageChange >= 500){
-        state = "ON";
-        lasttimeStageChange = currentTime;
-    }
-
     if(state == "ON"){
         digitalWrite(5,1);
+            if(currentTime - lasttimeStageChange >= 500){
+            state = "OFF";
+            lasttimeStageChange = currentTime;
+        }
     }
     else if(state == "OFF"){
         digitalWrite(5,0);
+        if(currentTime - lasttimeStageChange >= 500){
+            state = "ON";
+            lasttimeStageChange = currentTime;
+        }   
     }
-
+    Serial.println(currentTime);//6(1)แบบif(...&&...)   5(2)แบบ(ifซ้อนif)<---ซ้อนเร็วกว่า&&เฉย...
 }
