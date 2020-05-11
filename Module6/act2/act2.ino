1a5c8bb333d448ca996fc605b1d4c3f160d50e48
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-05-11 14:45:39 +0700

    act2

---
+++ Module6/act2/act2.ino
@@ -0,0 +1,24 @@
+long lastTimeStateChange =0;
+String state = "on";
+void setup(){
+    pinMode(5, OUTPUT);
+}
+void loop(){
+   //Serial.println(millis());
+   long currenTime = millis();
+   if(state =="on" && currenTime - lastTimeStateChange >= 500){
+       state ="off";
+       lastTimeStateChange = currenTime ;
+   }
+   else if(state =="off" && currenTime - lastTimeStateChange >= 500){
+       state ="on";
+       lastTimeStateChange = currenTime ; 
+   }
+    if (state =="on")
+    {
+        digitalWrite(5,1);
+    }
+    else if (state =="off")
+    {digitalWrite(5,0);
+    }
+}
