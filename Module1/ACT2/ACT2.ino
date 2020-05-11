a665ca243248b89e0302104d9412387654e47303
heartzontmove <taewan.thebigboss@mail.kmutt.ac.th>
2020-05-11 14:39:11 +0700

    FULL

--- Module1/ACT2/ACT2.ino
+++ Module1/ACT2/ACT2.ino
@@ -1,8 +1,30 @@
+long lasttimestatechange = 0;
+String state = "ON"
 void setup()
 {
     Serial.begin(9600);
+    pinMode(5, OUTPUT);
 }
 void loop()
 {
-    Serial.println(millis());
+    long currenttime = millis();
+    if (state == "ON" && currenttime - lasttimestatechange >= 500)
+    {
+        state = "OFF";
+        lasttimestatechange = currenttime;
+    }
+    else if (state == "OFF"&& currenttime - lasttimestatechange >= 500)
+    {
+        state = "ON";
+        lasttimestatechange = currenttime;
+    }
+
+    if (state == "ON")
+    {
+        digitalWrite(5,1);
+    }
+    else if (state == "OFF")
+    {
+        digitalWrite(5,0);
+    }
 }
