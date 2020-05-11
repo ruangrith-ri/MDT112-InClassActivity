76b7680225efa1925e23aed5a8099f07daeb4b75
intira <bluewhalechanal@gmail.com>
2020-05-11 15:10:34 +0700

    6-2

--- Modul6/6-2/6-2.ino
+++ Modul6/6-2/6-2.ino
@@ -1,33 +1,24 @@
 long lastTimeStateChange = 0;
 String state = "ON";
-
-void setup()
-{
+void setup(){
     pinMode(5, OUTPUT);
 }
-
-void loop()
-{
+void loop(){
     long currentTime = millis();
-
-    if(state == "ON" && currentTime - lastTimeStateChange>=500)
-    {
-        state = "OFF";
-        lastTimeStateChange=currentTime;
-    }
-    else if(state == "OFF"&& currentTime-lastTimeStateChange>=500)
-    {
-        state = "ON";
-        lastTimeStateChange=currentTime;
-    }
-
-
-    if(state=="ON")
-    {
-        digitalWrite(5, 1);
-    }
-    else if (state=="OFF") 
-    {
-        digitalWrite(5, 0);
+    
+    if(state == "ON"){ 
+    digitalWrite(5,1);
+        if(currentTime - lastTimeStateChange >= 500){
+            state = "OFF";
+            lastTimeStateChange = currentTime;
+            }
+        }
+    
+    if (state == "OFF"){
+    digitalWrite(5,0);
+        if(currentTime - lastTimeStateChange >= 500){
+            state = "ON";
+            lastTimeStateChange = currentTime;
+        }
     }
 }
