2f12d1617272f52df9b32ef82191ba1f82d4d6d5
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-05-11 14:38:44 +0700

    millis

---
+++ Act2/Act2.ino
@@ -0,0 +1,30 @@
+long lasttimeStrateChange = 0;
+String state = "ON";
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(5, OUTPUT);
+}
+void loop()
+{
+    long cuurrentTime = millis();
+
+    if(state == "ON" && cuurrentTime - lasttimeStrateChange >= 500)
+    {
+        state = "OFF";
+        lasttimeStrateChange = cuurrentTime;
+    }
+    else if (state == "OFF" && cuurrentTime - lasttimeStrateChange >= 500){
+        state = "ON";
+        lasttimeStrateChange - cuurrentTime;
+    }
+
+    if(state == "ON"){
+        digitalWrite(5, 1);
+        
+    }
+    else if (state == "OFF"){
+    digitalWrite(5, 0);
+    }
+}
