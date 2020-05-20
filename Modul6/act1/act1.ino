eb2aa8f5046c3141c2005b0ded1d30819701e12c
intira <bluewhalechanal@gmail.com>
2020-05-21 00:23:34 +0700

    act1

---
+++ Modul6/act1/act1.ino
@@ -0,0 +1,27 @@
+bool lastState= true;
+int x = 0;
+void setup()
+{
+    pinMode(2, INPUT_PULLUP);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    bool currentState = digitalRead(2);
+    if(lastState > currentState)
+    {
+        Serial.println("State FALLING"+String(x)+" ");
+        tone(8,500,500);
+        tone(8,500,500);
+        x++;
+    }
+    else if(lastState > currentState) 
+    {
+        Serial.println("State RISING"+String(x)+" ");
+        tone(8,400,500);
+        x++;
+    }
+    lastState = currentState;
+    delay(50);
+}
